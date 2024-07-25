5b0a309e737bd6117155a2a786db1e1b01d5d0a5
GreggtheDev <greggthedev@gmail.com>
2024-07-25 13:10:47 -0400

    created constructor to initialize product, cost & quantity

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -4,3 +4,10 @@ public class Product {
     private double cost;
     private int quantity;
     private String productName;
+
+    // Constructor to initialize product name, cost, and quantity
+    public Product(String productName, double cost, int quantity) {
+        this.productName = productName;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
