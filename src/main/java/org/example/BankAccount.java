b11bed0d5a96100f325622f108eb532325f2d979
GreggtheDev <greggthedev@gmail.com>
2024-07-24 22:52:14 -0400

    created constructor to initialize account holder name and balance

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -3,3 +3,9 @@ package org.example;
 public class BankAccount {
     private double balance;
     private String accountHolderName;
+
+    // Constructor to initialize account holder's name and balance
+    public BankAccount(String accountHolderName, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
